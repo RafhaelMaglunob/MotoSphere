6ef47ceaa7481bef6c421aad27c81349d74ed50c
Rafhael Maglunob <rafhaelmaglunob02@gmail.com>
2025-12-13 10:14:25 +0800

    React and Tailwind SETUP

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import UserLogin from './user/UserLogin'
+function App() {
+  return (
+    <>
+      <UserLogin></UserLogin>
+    </>
+  )
+}
+
+export default App
